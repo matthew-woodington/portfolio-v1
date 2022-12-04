c10b813505b8d8afa8ba4d7381ab446ee4829b81
matthew-woodington <smwoodington@gmail.com>
2022-12-04 16:52:47 -0500

    start projects

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,16 +1,24 @@
+import './projects.css'
+import reps from '../../assets/images/reps.png'
+
 function Projects() {
   return (
     <section id='projects' className='main-section'>
       <ul className="project-list">
         <li className="project-container">
           <div className="ex-image-container">
-            <img src="" alt="" className="ex-image" />
+            <a className='image-link' href="https://ccs-final-project-woodington.herokuapp.com/">
+              <img src={reps} alt="" className="ex-image" />
+            </a>
           </div>
           <div className="project-info">
-            <h2></h2>
-            <p></p>
+            <a href="https://ccs-final-project-woodington.herokuapp.com/"><h2>REPS Personal Training Tool</h2></a>
+            <p>Reps is a full stack application developed for personal trainers to market themselves to potential clients in their area, and manage those clients and their sessions.</p>
             <ul>
-              <li></li>
+              <li>React</li>
+              <li>Django</li>
+              <li>Django Rest Framework</li>
+              <li>Google Distance Matrix API</li>
             </ul>
           </div>
         </li>
