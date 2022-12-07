4282900b89f366d44b15809563c916c3260f717f
matthew-woodington <smwoodington@gmail.com>
2022-12-06 22:22:27 -0500

    changes card

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -2,24 +2,23 @@ import './projects.css'
 import reps from '../../assets/images/reps.png'
 
 function Projects() {
+
   return (
     <section id='projects' className='main-section'>
-      <ul className="project-list">
-        <li className="project-container">
-          <div className="ex-image-container">
-            <a className='image-link' href="https://ccs-final-project-woodington.herokuapp.com/">
-              <img src={reps} alt="" className="ex-image" />
-            </a>
-          </div>
-          <div className="project-info">
-            <a href="https://ccs-final-project-woodington.herokuapp.com/"><h2>REPS Personal Training Tool</h2></a>
-            <p>Reps is a full stack application developed for personal trainers to market themselves to potential clients in their area, and manage those clients and their sessions.</p>
-            <ul>
-              <li>React</li>
-              <li>Django</li>
-              <li>Django Rest Framework</li>
-              <li>Google Distance Matrix API</li>
-            </ul>
+      <ul className="cards" id='cards'>
+        <li className="card">
+          <div className="card-content">
+            <div className="card-image">
+              <img src="" alt="" />
+            </div>
+            <div className="card-info-wrapper">
+              <div className="card-info">
+                <div className="card-info-title">
+                  <h3><a href="https://ccs-final-project-woodington.herokuapp.com/">REPS Training Tool</a></h3>
+                  <h4>Full stack application developed for personal trainers to market themselves to potential clients in their area, and manage those clients and their sessions.</h4>
+                </div>
+              </div>
+            </div>
           </div>
         </li>
       </ul>
