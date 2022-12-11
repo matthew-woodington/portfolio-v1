bbbff74ef495a231c4a9a7bdcac57e7f4874f863
matthew-woodington <smwoodington@gmail.com>
2022-12-11 18:23:12 -0500

    project links

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -16,12 +16,12 @@ function Projects() {
             <h4 className='project-title'>REPS Application</h4>
             <p>REPS is a full stack application developed with ReactJS, Django, Django Rest Framework, React Bootstrap, and Google Maps Distance Matrix third party API. It is created for trainers to customize a profile and market themselves to local clientele as well as manage those clients and their scheduled sessions.</p>
             <ul className="project-links">
-              <li className="project-icon"><a href="https://github.com/matthew-woodington/ccs-final-project"><FiGithub /></a></li>
-              <li className="project-icon"><a href="https://ccs-final-project-woodington.herokuapp.com/"><FiExternalLink /></a></li>
+              <li className="project-icon"><a href="https://github.com/matthew-woodington/ccs-final-project" target='_blank'><FiGithub /></a></li>
+              <li className="project-icon"><a href="https://ccs-final-project-woodington.herokuapp.com/" target='_blank'><FiExternalLink /></a></li>
             </ul>
           </div>
           <div className="project-image">
-            <a href=""><img src={reps} alt="" className="img" /></a>
+            <a href="https://ccs-final-project-woodington.herokuapp.com/" target='_blank'><img src={reps} alt="" className="img" /></a>
           </div>
         </li>
         <li className="project">
@@ -29,12 +29,12 @@ function Projects() {
             <h4 className='project-title title-right'>Newspaper Application</h4>
             <p>This Newspaper Application is a full-stack news article management application developed with React JS, Django, and Django Rest Framework. The application allows a user to view news articles as well as create an account and submit their own articles for review by an admin that can then choose whether or not to publish those articles.</p>
             <ul className="project-links link-right">
-              <li className="project-icon"><a href="https://github.com/matthew-woodington/django-newspaper-app"><FiGithub /></a></li>
-              <li className="project-icon"><a href="https://newspaper-app-mwoodington.herokuapp.com/"><FiExternalLink /></a></li>
+              <li className="project-icon"><a href="https://github.com/matthew-woodington/django-newspaper-app" target='_blank'><FiGithub /></a></li>
+              <li className="project-icon"><a href="https://newspaper-app-mwoodington.herokuapp.com/" target='_blank'><FiExternalLink /></a></li>
             </ul>
           </div>
           <div className="project-image image-left">
-            <a href=""><img src={newspaper} alt="" className="img" /></a>
+            <a href="https://newspaper-app-mwoodington.herokuapp.com/" target='_blank'><img src={newspaper} alt="" className="img" /></a>
           </div>
         </li>
         <li className="project">
@@ -42,12 +42,12 @@ function Projects() {
             <h4 className='project-title'>Pixel Perfect</h4>
             <p>Pixel Perfect is a website recreation that uses base HTML and CSS to replicate an image of a pre-existing website in both desktop and mobile views.</p>
             <ul className="project-links">
-              <li className="project-icon"><a href="https://github.com/matthew-woodington/pixel-perfect"><FiGithub /></a></li>
-              <li className="project-icon"><a href="https://matthew-woodington.github.io/pixel-perfect/"><FiExternalLink /></a></li>
+              <li className="project-icon"><a href="https://github.com/matthew-woodington/pixel-perfect" target='_blank'><FiGithub /></a></li>
+              <li className="project-icon"><a href="https://matthew-woodington.github.io/pixel-perfect/" target='_blank'><FiExternalLink /></a></li>
             </ul>
           </div>
           <div className="project-image">
-            <a href=""><img src={pixel} alt="" className="img" /></a>
+            <a href="https://matthew-woodington.github.io/pixel-perfect/" target='_blank'><img src={pixel} alt="" className="img" /></a>
           </div>
         </li>
       </ul>
