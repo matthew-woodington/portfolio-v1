01f1bb08ee982677ac64506bfd95797805cf56ba
matthew-woodington <smwoodington@gmail.com>
2022-12-12 20:28:25 -0500

    design for project highlight

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -10,7 +10,7 @@ function Projects() {
   return (
     <section id='projects' className='main-section'>
       <h1 className="projects-title">Highlighted Projects</h1>
-      <ul className="project-list">
+      {/* <ul className="project-list">
         <li className="project">
           <div className="project-info">
             <h4 className='project-title'>REPS Application</h4>
@@ -50,6 +50,108 @@ function Projects() {
             <a href="https://matthew-woodington.github.io/pixel-perfect/" target='_blank'><img src={pixel} alt="" className="img" /></a>
           </div>
         </li>
+      </ul> */}
+
+      <ul className="project-list">
+
+        <li className="project">
+          <div className="project-num">
+            <span className='num'>01</span><span className='dot'>.</span>
+          </div>
+          <div className="project-card">
+            <div class="card">
+              <div class="card-img">
+                <img className='img' src={reps} alt="" />
+                <div class="overlay">
+                  <div class="overlay-content">
+                    <h2>REPS Application</h2>
+                    <p>REPS is a full stack application developed for trainers to customize a profile and market themselves and connect with local clientele. The application also functions as a managemant tool to allow trainers to manage those clients and their scheduled sessions in various ways.</p>
+                    <ul className="frameworks">
+                      <li>React.JS</li>
+                      <li>Django</li>
+                      <li>Django Rest Framework</li>
+                      <li>Google Distance Matrix</li>
+                    </ul>
+                    <div className="overlay-links">
+                      <a class="hover" href="https://github.com/matthew-woodington/ccs-final-project" target='_blank'><FiGithub className='hover-icon' /></a>
+                      <a class="hover" href="https://ccs-final-project-woodington.herokuapp.com/" target='_blank'><FiExternalLink className='hover-icon' /></a>
+                    </div>
+                  </div>
+              </div>
+            </div>
+    
+            <div class="card-content">
+                  <h2>REPS Application</h2>
+              </div>
+            </div>
+          </div>
+        </li>
+
+        <li className="project">
+          <div className="project-card card-even">
+            <div class="card">
+              <div class="card-img">
+                <img className='img' src={newspaper} alt="" />
+                <div class="overlay">
+                  <div class="overlay-content">
+                    <h2>Newspaper Application</h2>
+                    <p>This Newspaper Application is a full-stack news article management application. The application allows a user to view news articles as well as create an account and submit their own articles for review by an admin that can then choose whether or not to publish those articles.</p>
+                    <ul className="frameworks">
+                      <li>React.JS</li>
+                      <li>Django</li>
+                      <li>Django Rest Framework</li>
+                    </ul>
+                    <div className="overlay-links">
+                      <a class="hover" href="https://github.com/matthew-woodington/django-newspaper-app" target='_blank'><FiGithub className='hover-icon' /></a>
+                      <a class="hover" href="https://newspaper-app-mwoodington.herokuapp.com/" target='_blank'><FiExternalLink className='hover-icon' /></a>
+                    </div>
+                  </div>
+              </div>
+            </div>
+    
+            <div class="card-content">
+                  <h2>Newspaper Application</h2>
+              </div>
+            </div>
+          </div>
+          <div className="project-num num-even">
+            <span className='num'>02</span><span className='dot'>.</span>
+          </div>
+        </li>
+
+        <li className="project">
+          <div className="project-num">
+            <span className='num'>03</span><span className='dot'>.</span>
+          </div>
+          <div className="project-card">
+            <div class="card">
+              <div class="card-img">
+                <img className='img' src={pixel} alt="" />
+                <div class="overlay">
+                  <div class="overlay-content">
+                    <h2>Pixel Perfect</h2>
+                    <p>Pixel Perfect is a website recreation based on a previously existing website. It is created using vanilla HTML and CSS to replicate images of both a desktop and mobile version of the example website.</p>
+                    <ul className="frameworks">
+                      <li>HTML</li>
+                      <li>CSS</li>
+                      <li>JavaScript</li>
+                    </ul>
+                    <div className="overlay-links">
+                      <a class="hover" href="https://github.com/matthew-woodington/pixel-perfect" target='_blank'><FiGithub className='hover-icon' /></a>
+                      <a class="hover" href="https://matthew-woodington.github.io/pixel-perfect/" target='_blank'><FiExternalLink className='hover-icon' /></a>
+                    </div>
+                  </div>
+              </div>
+            </div>
+    
+            <div class="card-content">
+                  <h2>Pixel Perfect</h2>
+              </div>
+            </div>
+          </div>
+        </li>
+
+
       </ul>
     </section>
   )
