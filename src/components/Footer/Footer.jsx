62ad47f28ff51a34cc95515d6a1c0e3acac8692e
matthew-woodington <smwoodington@gmail.com>
2022-12-15 17:19:05 -0500

    build

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import './footer.css'
 function Footer() {
   return (
     <section className='footer'>
-      <p><a className='portfolio-git' href="https://github.com/matthew-woodington/portfolio-v1">Built by Matthew Woodington, 2022</a></p>
+      <p><a className='portfolio-git' href="https://github.com/matthew-woodington/portfolio-v1" target='_blank' rel="noreferrer">Built by Matthew Woodington, 2022</a></p>
     </section>
   )
 }
